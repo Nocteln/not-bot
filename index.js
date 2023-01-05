99d472aa86939e6fbae113afcbc38bc522d6f861
Guscraftin <augustin.glorian@outlook.com>
2023-01-05 21:57:37 +0100

    Add process
    Signed-off-by: Guscraftin <augustin.glorian@outlook.com>

--- index.js
+++ index.js
@@ -10,5 +10,19 @@ client.commands = new Collection();
     require(`./handlers/${handler}`)(client);
 });
 
+process.on('exit', code => { Logger.client(`Le processus s'est arrêté avec le code : ${code}`) });
+
+process.on('uncaughtException', (err, origin) => { 
+    Logger.error(`UNCAUGHT_EXCEPTION : ${err}`)
+    console.error(`Origine : ${origin}`)
+});
+
+process.on('unhandledRejection', (reason, promise) => { 
+    Logger.warn(`UNHANDLE_REJECTION : ${{reason}}`)
+    console.log(promise);
+});
+
+process.on('warning', (...args) => { Logger.warn(...args) });
+
 
 client.login(process.env.TOKEN);
