171a9c924b24d654dade197b9ca4737584608e3e
Eliott <eliottmieze@gmail.com>
2023-05-28 19:21:48 +0200

    début de la commande /lock

---
+++ src/commands/admin/lock.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const { embedr } = require("../../fonctions/embed");
+
+module.exports = {
+  data: new Discord.SlashCommandBuilder()
+    .setName("lock")
+    .setDMPermission(false)
+    .setDescription("Permet de retirer aux membres la permissions d'écrire")
+    .setDefaultMemberPermissions(Discord.PermissionFlagsBits.ManageChannels)
+    .addChannelOption((o) =>
+      o.setName("salon").setDescription("Le salon que vous voulez bloquer")
+    )
+    .addRoleOption((o) =>
+      o.setName("role").setDescription("Le role que vous voulez bloquer")
+    ),
+
+  async execute(interaction) {
+    const channel =
+      interaction.options.getChannel("salon") ||
+      interaction.guild.channels.cache.get(interaction.channelId);
+
+    const role = interaction.options.getRole("role");
+    if (role && !interaction.guild.roles.cache.get(role.id))
+      return interaction.reply({
+        embeds: [embedr("Red", "❌ erreur", "le rôle n'as pas été trouvé !")],
+      });
+    if (!role) role = interaction.guild.roles.everyone;
+
+    if (channel.type !== 0)
+      return interaction.reply({
+        embeds: [
+          embedr("Red", ":x: erreur", "Veuillez spécifier un salon textuel!"),
+        ],
+      });
+
+    channel.permissionOverwrites;
+  },
+};
